97d4fb7abc5bea40972d4df39b69990c806fd9f6
Skylar Saveland <skylar.saveland@gmail.com>
2018-06-11 15:49:58 -0700

    oops ... should have used reflect instead of one-off function ...

--- src/pango/lib/sliceutil/compare.go
+++ src/pango/lib/sliceutil/compare.go
@@ -1,6 +1,8 @@
 package sliceutil
 
-
+// EqualInt is a deprecated function that takes two slices of ints and returns
+// a boolean of whether the two slices have equal values at every position
+// instead, use reflect.DeepEqual https://stackoverflow.com/a/15312182
 func EqualInt(a, b []int) bool {
     if a == nil && b == nil {
         return true;
