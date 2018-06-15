8d5dac9e61b375485a525e852410762c68971ad7
Skylar Saveland <skylar.saveland@gmail.com>
2018-06-15 13:01:01 -0700

    goa example building with go cli - try to get it into Bazel

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
+# Prerequisites
+
+Assuming you have the following installed:
+
+* Bazel
+
+
+# Install Dependencies
+
+```
+# TODO: why can't we put the .toml and lock file in the root?
+cd src/pango
+dep ensure
+cd ../..
+```
+
+
 # Test
 
 ```
 bazel test //...
 ```
+
 
 # Build
 
---
+++ src/pango/services/cellar/design/design.go
@@ -0,0 +1,43 @@
+// see https://goa.design/learn/guide/
+package design
+
+import (
+  . "github.com/goadesign/goa/design"
+  . "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = API("cellar", func() {
+  Title("The Wine Cellar")
+  Description("Example")
+  Scheme("http")
+  Host("localhost:8080")
+})
+
+var _ = Resource("bottle", func() {
+  BasePath("/bottles")
+  DefaultMedia(BottleMedia)
+  Action("show", func() {
+    Description("Get bottle by id")
+    Routing(GET("/:bottleID"))
+    Params(func() {
+      Param("bottleID", Integer, "Bottle ID")
+    })
+    Response(OK)
+    Response(NotFound)
+  })
+})
+
+var BottleMedia = MediaType("application/vnd.goa.example.bottle+json", func(){
+  Description("A bottle of wine")
+  Attributes(func() {
+    Attribute("id", Integer, "Unique bottle ID")
+    Attribute("href", String, "API href for making requests on the bottle")
+    Attribute("name", String, "Name of wine")
+    Required("id", "href", "name")
+  })
+  View("default", func() {
+    Attribute("id")
+    Attribute("href")
+    Attribute("name")
+  })
+})
